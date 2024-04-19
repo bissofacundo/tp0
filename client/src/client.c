5a8f377ab744729dce536dba1a333454f465cc7a
Facundo Bisso <fbisso@frba.utn.edu.ar>
2024-04-19 03:47:13 +0000

    Primer avance

--- client/src/client.c
+++ client/src/client.c
@@ -16,15 +16,23 @@ int main(void)
 
 	logger = iniciar_logger();
 
+	if (!logger){
+		terminar_programa(conexion,logger,config);
+		return 1;
+	}
+
 	// Usando el logger creado previamente
 	// Escribi: "Hola! Soy un log"
 
-	log_info(logger, "Soy un log");
+	log_info(logger, "Hola! Soy un log");
 
 	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
 
-	 config = iniciar_config();
-
+	config = iniciar_config();
+	if (!config){
+		terminar_programa(conexion,logger,config);
+		return 2;
+	}
 	// Usando el config creado previamente, leemos los valores del config y los 
 	// dejamos en las variables 'ip', 'puerto' y 'valor'
 
@@ -39,18 +47,20 @@ int main(void)
 
 	/* ---------------- LEER DE CONSOLA ---------------- */
 
-	// leer_consola(logger);
+	leer_consola(logger);
 
 	/*---------------------------------------------------PARTE 3-------------------------------------------------------------*/
 
 	// ADVERTENCIA: Antes de continuar, tenemos que asegurarnos que el servidor esté corriendo para poder conectarnos a él
-
+	ip = config_get_string_value(config, "IP");
+	puerto  = config_get_string_value(config, "PUERTO");
 	// Creamos una conexión hacia el servidor
-	// conexion = crear_conexion(ip, puerto);
+	conexion = crear_conexion(ip, puerto);
 
 	// Enviamos al servidor el valor de CLAVE como mensaje
-
+	enviar_mensaje(valor, conexion);
 	// Armamos y enviamos el paquete
+	
 	// paquete(conexion);
 
 	terminar_programa(conexion, logger, config);
@@ -63,7 +73,7 @@ t_log* iniciar_logger(void)
 {
 	t_log* nuevo_logger;
 
-	nuevo_logger = log_create("/home/fbisso/repos/so/tp0/client/tp0.log", "Proceso de TP0", true, LOG_LEVEL_INFO);
+	nuevo_logger = log_create("/home/utnso/repos/tp0/client/tp0.log", "Proceso de TP0", true, LOG_LEVEL_INFO);
 
 	return nuevo_logger;
 }
@@ -72,7 +82,7 @@ t_config* iniciar_config(void)
 {
 	t_config* nuevo_config;
 
-	nuevo_config = config_create("/home/fbisso/repos/so/tp0/client/cliente.config");
+	nuevo_config = config_create("/home/utnso/repos/tp0/client/cliente.config");
 
 	return nuevo_config;
 }
@@ -82,13 +92,15 @@ void leer_consola(t_log* logger)
 	char* leido;
 
 	// La primera te la dejo de yapa
-	//leido = readline("> ");
+	leido = readline("> ");
 
+	while(strcmp(leido,"")){
 	// El resto, las vamos leyendo y logueando hasta recibir un string vacío
-
-
+		log_info(logger, leido);
 	// ¡No te olvides de liberar las lineas antes de regresar!
-
+		free(leido);
+		leido = readline("> ");
+	}
 }
 
 void paquete(int conexion)
@@ -108,6 +120,8 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
-	log_destroy(logger);
-	config_destroy(config);
+	if (logger)
+		log_destroy(logger);
+	if (config)
+		config_destroy(config);
 }
